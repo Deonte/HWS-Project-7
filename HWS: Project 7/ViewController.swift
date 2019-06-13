24a975a3e5ab41da2ff3793fb2d42d45af5bd18b
Deonte <dkill51@gmail.com>
2019-06-13 18:18:19 -0400

    Project 7 Challenge. 1. Add credits button letting user know where the data is coming from. --- Complete 2. Let user filter through results for petitions. --*Incomplete*--

--- HWS: Project 7/ViewController.swift
+++ HWS: Project 7/ViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Challenge 1: Add Credits button to the top right
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .done, target: self, action: #selector(handleCreditsButtonPressed))
+        
+        // Challenge 2: Let users filter the petitions they see.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterResults))
         
         let urlString: String
         
@@ -32,7 +38,7 @@ class ViewController: UITableViewController {
                 return
             }
         }
-        handleError()
+        handleAlert(title: "Error Loading Page", message: "We are having trouble loading the Petition Data. Please ensure your internet connection is working.")
     }
     
     func parse(json: Data) {
@@ -40,15 +46,43 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
-    func handleError() {
-        let ac = UIAlertController(title: "Error Loading Page", message: "We are having trouble loading the Petition Data. Please ensure your internet connection is working.", preferredStyle: .alert)
+    @objc func handleAlert(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
         
         present(ac, animated: true)
+    }
+    
+    @objc func handleCreditsButtonPressed() {
+        if navigationController?.tabBarItem.tag == 0 {
+            handleAlert(title: "Sample Data Provided by.", message: "www.hackingwithswift.com")
+        } else {
+            handleAlert(title: "Live Data Provided by.", message: "We the people API of the WhiteHouse.")
+        }
+        
+    }
+    
+    @objc func filterResults() {
+        let ac = UIAlertController(title: "Search Petitions", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let filteringPetitions = UIAlertAction(title: "Search", style: .default) { [weak self, weak ac] _ in
+            guard let searchText = ac?.textFields?[0].text else {return}
+            self?.search(searchText)
+        }
+        
+        ac.addAction(filteringPetitions)
+        present(ac, animated: true)
+    }
+    
+    func search(_ search: String) {
+        
+        // Challenge 2: Incomplete but will return later to finish.
     }
     
 }
@@ -63,7 +97,7 @@ extension ViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         
@@ -72,7 +106,7 @@ extension ViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
     
